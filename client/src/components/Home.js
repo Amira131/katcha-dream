41c44762bd45f62983b9e47e4e5a6f175afd8e7c
Amira de Moya <demoyaamira@gmail.com>
2023-01-23 13:00:35 -0500

    updated  Nav and Sidebar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 // import dream_catcher from './images/dream-catcher.png' 
 import './home.css'
 import Navbar from './Navbar'   
-// import Sidebar from './Sidebar' 
+import Sidebar from './Sidebar' 
 import my_movie from '../videos/my_movie.mp4'
 import catcher from '../images/catcher.png'
 import Logo from './Logo'
@@ -13,68 +13,28 @@ const Home = () => {
 
   return (
     <div >
-        {/* <Navbar />  */}
-        {/* <Sidebar />  */}
+         <Navbar /> 
+         <Sidebar />  
        
         <div className="main">
             
             {/* <div className="overlay"></div> */}
             <video src={my_movie} autoPlay loop muted />
-            background-image: url{catcher}
+{/*             
+            <img src={catcher} alt="" /> */}
                 
                 <div className='content'>
                      
                     <h1>Welcome</h1>
-                    <h2>To</h2>
+                    <h2>to</h2>
                     <h1>Katcha Dream</h1>
             
                      
-                </div>     
-                   
-                    
-                
-                
-                
-                
-            
-                
-               
-
-                
-                
+                </div>   
+             </div>
         </div>
-       
-
-    </div>
         )
     }
-
-    //  font-size: 80px;
-    
-   
-//    <div className='logo'>
-//                      
-//                  </div>
     export default Home
 
-//     img{
-    
-//     width:  500px;
-//     height: 600px;
-//     display: flexbox;
-    
-          
-//   }
-// img{
-    
-//     width:  500px;
-//     height: 600px;
-//     display: flexbox;
-    
-          
-//   }
-// 
-          
-//   }
-
-// }
+ 
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './sidebar.css';
+import catcher from '../images/catcher.png'
 
 const Sidebar = () => {
   return (
+
+
     <div class="sidebar">
+      
   <a class="active" href="#home">Home</a>
   <a href="#about">About</a>
   <a href="#new_entry">New Entry</a>
   <a href="#entries">Entries</a>
   <a href="#logout">Logout</a>
-</div>
+  
+  </div>
+
   
 
   )
